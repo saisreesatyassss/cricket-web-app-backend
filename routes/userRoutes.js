25aeeefb56a714567f229762568565acfdc9e341
saisreesatyassss <saisreesatyassss@gmail.com>
2025-05-06 01:38:16 +0530

    profilePicture

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -74,7 +74,7 @@ router.get("/profile", authMiddleware, async (req, res) => {
       profilePage: {
         firstName: user.profilePage.firstName,
         lastName: user.profilePage.lastName,
-        profilePicture: user.profilePage.profilePicture,
+        // profilePicture: user.profilePage.profilePicture,
         education: user.profilePage.education,
         gender: user.profilePage.gender,
         stateOfResidence: user.profilePage.stateOfResidence,
